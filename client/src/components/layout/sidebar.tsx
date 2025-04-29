0a51a5afc093128604566d3396f156054704d22f
Dushi1001 <ishara.hiphop@gmail.com>
2025-04-29 07:54:56 +0400

    Update sidebar.tsx

--- client/src/components/layout/sidebar.tsx
+++ client/src/components/layout/sidebar.tsx
@@ -1,5 +1,3 @@
-// This is a redirect file to handle case-sensitivity issues
-// It simply re-exports the SidebarComponent to maintain compatibility
-
-import Sidebar from "./SidebarComponent";
-export default Sidebar;
+git add client/src/components/layout/sidebar.tsx
+git commit -m "Add sidebar compatibility layer"
+git push origin main
